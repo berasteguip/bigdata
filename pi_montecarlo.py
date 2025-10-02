2b68dd903fb6f4b157cd90f79868da2a64f2ff5f
Pablo <202311460@alu.comillas.edu>
2025-10-02 19:29:39 +0200

    pi_montecarlo

---
+++ pi_montecarlo.py
@@ -0,0 +1,23 @@
+from random import random
+from typing import Tuple
+
+# area circulo / area cuadrado = nº puntos en circulo / nº puntos en cuadrado
+# pi * r^2 / 4r^2 = nº puntos en circulo / nº puntos en cuadrado
+# pi = nº puntos en circulo * 4 / nº puntos en cuadrado
+
+# Cuadrado de 1x1
+
+def ratio(point: Tuple[float, float]) -> float:
+    return point[0]**2 + point[1]**2
+
+def get_pi(n=1000) -> float:
+
+    points_in_square = [(random(), random()) for _ in range(n)]
+    points_in_circle = [point for point in points_in_square if ratio(point) < 1]
+
+    return 4 * len(points_in_circle) / len(points_in_square)
+
+
+if __name__ == "__main__":
+
+    print(get_pi(1000000))
