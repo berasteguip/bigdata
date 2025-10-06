cc791ea243de931bd613e216562518316536b75e
Pablo <202311460@alu.comillas.edu>
2025-10-06 13:04:15 +0200

    ejecucion

--- ejecucion.sh
+++ ejecucion.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+# Archivo CSV de salida
+output="resultados.csv"
+
+# Escribe la cabecera
+echo "n,r,resultado" > "$output"
+
+# Bucle doble
+for n in 5000000 10000000 50000000; do
+    echo "Ejecutando con n=$n y iter=1..."
+    
+    # Ejecutar el script de Python y capturar su salida
+    result=$(python3 pi_montecarlo.py "$n" "1")
+    
+    # Añadir una línea al CSV
+    echo "$n,$result" >> "$output"
+  done
+done
+
+echo "Ejecuciones completadas. Resultados guardados en $output"
